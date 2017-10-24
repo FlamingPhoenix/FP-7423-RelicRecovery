f159daeb13c70a6338a4890f6e197ccee3f626cf
HwaA18 <HwaA18@gmail.com>
2017-10-24 19:47:16 -0400

    added new files: flamingphoenix class and an imu test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PixyTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.I2cDevice;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+
+import org.firstinspires.ftc.teamcode.Library.PixyCam;
+
+/**
+ * Created by HwaA1 on 10/22/2017.
+ */
+
+@Autonomous(name = "pixytest", group = "none")
+public class PixyTest extends LinearOpMode {
+
+    I2cDeviceSynch pix;
+
+    PixyCam pixy;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //pix = hardwareMap.get(I2cDeviceSynch.class, "pixy");
+
+        pixy = hardwareMap.get(PixyCam.class, "pixy");
+
+        waitForStart();
+
+        while(this.opModeIsActive()) {
+            //  pixy.GetBiggestBlock();
+
+            telemetry.addData("pixy blue", pixy.GetBiggestBlock(2));
+            telemetry.addData("pixy red", pixy.GetBiggestBlock(1));
+            telemetry.addData("null", pixy.GetBiggestBlock(3));
+            telemetry.update();
+        }
+    }
+}
